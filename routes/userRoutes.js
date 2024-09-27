400b85199ac2b36fe6f6ae8c153fb9c9e161b55a
praveensonesha <praveensonesha2003@gmail.com>
2024-09-27 11:11:14 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -10,6 +10,7 @@ const {signUpUser,loginUser} = require('../controllers/userController.js');
 // Define user routes
 router.post('/signup',signUpUser);
 router.post('/login',loginUser);
+// router.post('/request',authenticateToken,requestuser);
 
 
 module.exports = router;
